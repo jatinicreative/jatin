d063eb0f00c31dd3511803e1656fc27d40560401
Jatin <jatin@icreativetechnolabs.com>
2025-05-02 18:50:02 +0530

    Added

---
+++ custom/plugins/SwagBlog/src/Core/Controllers/Storefront/ExampleController.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace SwagBlog\Core\Controllers\Storefront;
+
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Storefront\Controller\StorefrontController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route(defaults: ['_routeScope' => ['storefront']])]
+class ExampleController extends StorefrontController
+{
+
+    private EntityRepository $blogRepository;
+
+    public function __construct(EntityRepository $blogRepository)
+    {
+        $this->blogRepository = $blogRepository;
+    }
+
+    #[Route(path: '/hello-world',name: 'storefront.example.show', methods: ['GET'])]
+    public function showExample(Context $context): Response
+    {
+        $blogs = $this->blogRepository->search(new Criteria(), $context)->getEntities();
+
+        return $this->renderStorefront('@SwagBlog/storefront/page/example.html.twig', [
+            'example' => 'Hello world',
+            'blogs' => $blogs,
+        ]);
+    }
+}
