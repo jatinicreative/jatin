f77a616843ac4ca6e05a5f350216f01b58b1cb2d
Jatin <jatin@icreativetechnolabs.com>
2025-05-08 11:12:04 +0530

    Added

---
+++ custom/plugins/Blog/src/Blog.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Blog;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class Blog extends Plugin implements ThemeInterface
+{
+}
