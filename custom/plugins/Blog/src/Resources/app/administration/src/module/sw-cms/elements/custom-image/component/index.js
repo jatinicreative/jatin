cea3a1a7e22e9541bdd84b2a6b2e788e0e031fa3
Jatin <jatin@icreativetechnolabs.com>
2025-05-12 19:19:05 +0530

    Added

--- custom/plugins/Blog/src/Resources/app/administration/src/module/sw-cms/elements/custom-image/component/index.js
+++ custom/plugins/Blog/src/Resources/app/administration/src/module/sw-cms/elements/custom-image/component/index.js
@@ -12,40 +12,31 @@ Component.register('sw-cms-el-custom-image', {
     ],
 
     computed: {
-
         mediaUrl() {
             const fallBackImageFileName = CMS.MEDIA.previewMountain.slice(CMS.MEDIA.previewMountain.lastIndexOf('/') + 1);
             const staticFallBackImage = this.assetFilter(`administration/static/img/cms/${fallBackImageFileName}`);
-            const elemData = this.element.data.media;
-            const elemConfig = this.element.config.media;
+            const elemData = this.element?.data?.media || {};
+            const elemConfig = this.element?.config?.media || {};
 
             if (elemConfig.source === 'mapped') {
                 const demoMedia = this.getDemoValue(elemConfig.value);
-
                 if (demoMedia?.url) {
                     return demoMedia.url;
                 }
-
                 return staticFallBackImage;
             }
 
-            if (elemConfig.source === 'default') {
-
+            if (elemConfig.source === 'default' && typeof elemConfig.value === 'string') {
                 const fileName = elemConfig.value.slice(elemConfig.value.lastIndexOf('/') + 1);
                 return this.assetFilter(`/administration/static/img/cms/${fileName}`);
             }
 
-            if (elemData?.id) {
-                return this.element.data.media.url;
-            }
-
             if (elemData?.url) {
-                return this.assetFilter(elemConfig.url);
+                return elemData.url;
             }
 
             return staticFallBackImage;
         },
-
 
         assetFilter() {
             return Filter.getByName('asset');
@@ -57,7 +48,6 @@ Component.register('sw-cms-el-custom-image', {
     },
 
     watch: {
-
         mediaConfigValue(value) {
             const mediaId = this.element?.data?.media?.id;
             const isSourceStatic = this.element?.config?.media?.source === 'static';
